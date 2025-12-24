70ddb888e78412ec9a54954b18fecb1419da9e76
ksr2code <ksr2code@gmail.com>
2025-12-24 01:22:54 +0100

    updated parser to handle exceptions gracefully

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -3,17 +3,31 @@
 Main module
 """
 
-from sys import argv
-from parser import ConfigParser
+from sys import argv, exit
+from parser import ConfigParser, ParsingError
 from visualizer import Visualizer
 
 
 def a_maze_ing(argv: list[str]):
+    if len(argv) != 2:
+        print("Usage: python3 a_maze_ing.py config.txt")
+        exit(1)
+    config_file = argv[1]
     parser = ConfigParser()
     try:
-        parser.parse("config_vis.txt")
+        parser.parse(config_file)
+    except FileNotFoundError as e:
+        print(f"Error: Configuration file not found: {config_file}")
+        exit(1)
+    except ParsingError as e:
+        print(f"Configuration Error: {e}")
+        exit(1)
+    except ValueError as e:
+        print(f"Invalid value in configuration: {e}")
+        exit(1)
     except Exception as e:
-        print("Error: ", e)
+        print(f"Unexpected error: {e}")
+        exit(1)
 
     vis = Visualizer()
     vis.read(parser.output_file)
